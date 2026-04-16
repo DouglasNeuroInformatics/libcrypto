5733c3a3a27bc501a493ddada231cb8bd8f34fcf
joshunrau <contact@joshuaunrau.com>
2026-04-16 11:42:11 -0400

    chore: move exports last

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import type { webcrypto } from 'node:crypto';
 
-export * from './encryption.js';
-export * from './hash.js';
-
 declare global {
   type CryptoKey = webcrypto.CryptoKey;
   type CryptoKeyPair = webcrypto.CryptoKeyPair;
   const CryptoKey: webcrypto.CryptoKeyConstructor;
 }
+
+export * from './encryption.js';
+export * from './hash.js';
